f3ee704a861bfcf38eb80aaf40523c8062c6aa56
Tadi <adam.tomko@gmail.com>
2016-05-01 20:07:45 +0200

    basic dependencies and app file

---
+++ app.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var app = express();
+
+app.set('view engine', 'ejs');
+
+app.use(express.static('public'));
+app.use('/css', express.static(__dirname + '/node_modules/bootstrap/dist/css'));
+app.use('/js', express.static(__dirname + '/node_modules/bootstrap/dist/js'));
+app.use('/fonts', express.static(__dirname + '/node_modules/bootstrap/dist/fonts'));
+app.use('/js', express.static(__dirname + '/node_modules/jquery/dist'));
+app.listen(3000, function () {
+    console.log('Example app listening on port 3000!');
+});
+
+
